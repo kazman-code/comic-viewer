cfba866c5e76bcc091eb6e9c20cb25e0a328ca4c
Mick_code <mickdonnellan2@gmail.com>
2026-04-23 21:39:32 +0100

    Improved homepage and herodetails page

--- src/app/pages/heroes/heroes.page.ts
+++ src/app/pages/heroes/heroes.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { IonContent, IonHeader, IonToolbar, IonTitle } from '@ionic/angular/standalone';
+import { FormsModule } from '@angular/forms';
+import { IonContent, IonHeader, IonToolbar, IonTitle, IonSearchbar, IonCard, IonItem, IonAvatar, IonLabel } from '@ionic/angular/standalone';
 import { Router } from '@angular/router';
 import { RouterLink } from '@angular/router';
 
@@ -12,25 +13,33 @@ import { Comic } from '../../services/comic.service';
   templateUrl: './heroes.page.html',
   styleUrls: ['./heroes.page.scss'],
   imports: [
-    IonContent,
-    IonHeader,
-    IonTitle,
-    IonToolbar,
-    CommonModule, RouterLink
-  ]
+    IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, RouterLink, FormsModule, IonSearchbar, IonCard, IonItem, IonAvatar, IonLabel ]
 })
 export class HeroesPage implements OnInit {
   comics: any[] = [];
   loading = true;
+  allComics: any[] = [];
+  filteredComics: any[] = [];
+  searchTerm = '';
 
   constructor(private comic: Comic, private router: Router) {}
 
   ngOnInit() {
     this.comic.getComics().subscribe(data => {
-      this.comics = data;
+      this.allComics = data;
+      this.filteredComics = data;
       this.loading = false;
     });
   }
+
+  filterComics() {
+  const term = this.searchTerm.toLowerCase();
+
+  this.filteredComics = this.allComics.filter(comic =>
+    comic.name.toLowerCase().includes(term)
+  );
+}
+
 
   openDetail(id: number) {
     this.router.navigate(['/hero-detail', id]);
--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonCard, IonCardHeader, IonCardTitle, IonCardContent } from '@ionic/angular/standalone';
 import { RouterLink } from '@angular/router';
 import { Storage } from '@ionic/storage-angular';
+import { Comic } from 'src/app/services/comic.service';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 
 
@@ -10,18 +13,27 @@ import { Storage } from '@ionic/storage-angular';
   standalone: true,
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [ IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonCard, IonCardHeader, IonCardTitle, IonCardContent, RouterLink
-  ],
+  imports: [ IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonCard, IonCardHeader, IonCardTitle, IonCardContent, RouterLink, CommonModule ],
 })
 export class HomePage {
+  characters: any[] = [];
 
-  constructor(private storage: Storage) {}
+  constructor(
+    private storage: Storage,
+    private comic: Comic,
+    private router: Router
+  ) {}
+  
 
   async ngOnInit() {
-  await (this.storage as any).create();
-  await this.storage.set('test', 'hello');
-  const value = await this.storage.get('test');
-  console.log('Storage test:', value);
+  await this.storage.create();
+ this.comic.getComics().subscribe(data => {
+      this.characters = data.slice(0, 12); // show first 12 characters
+    });
+  }
+
+  openHero(id: number) {
+    this.router.navigate(['/hero-detail', id]);
 }
 
 }
